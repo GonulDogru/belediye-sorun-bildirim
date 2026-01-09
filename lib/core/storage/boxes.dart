434fd527b31c00b96c3f94cb943ec73c95010be6
GonulDogru <gonuldgr33@gmail.com>
2026-01-10 00:26:32 +0300

    refactor: Hive box adlari Boxes sabitlerinde toplandi

---
+++ lib/core/storage/boxes.dart
@@ -0,0 +1,27 @@
+class Boxes {
+  Boxes._();
+
+  static const String users = 'users';
+  static const String sessions = 'sessions';
+  static const String profiles = 'profiles';
+  static const String categories = 'categories';
+  static const String issues = 'issues';
+  static const String issuePhotos = 'issue_photos';
+  static const String issueStatusHistory = 'issue_status_history';
+  static const String issueComments = 'issue_comments';
+  static const String municipalityWorks = 'municipality_works';
+  static const String announcements = 'announcements';
+
+  static const List<String> all = [
+    users,
+    sessions,
+    profiles,
+    categories,
+    issues,
+    issuePhotos,
+    issueStatusHistory,
+    issueComments,
+    municipalityWorks,
+    announcements,
+  ];
+}
